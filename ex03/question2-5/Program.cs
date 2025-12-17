99badb1591768c323c83eb71584956eec21b721c
רות <rut6506@email.com>
2025-12-17 21:15:31 +0200

    Added ex05 exercises

---
+++ ex03/question2-5/Program.cs
@@ -0,0 +1,80 @@
+﻿//2
+public class ArrayProcessor
+{
+    public delegate void UnaryAction(double a);
+
+    public static void ProcessArray(double[] array, UnaryAction processor)
+    {
+        foreach (double value in array)
+        {
+            processor(value);
+        }
+    }
+}
+//3
+public class SumCalculator
+{
+    public double Sum { get; private set; } = 0;
+
+    public void AddToSum(double value)
+    {
+        Sum += value;
+    }
+}
+
+public class MaxCalculator
+{
+    public double Max { get; private set; } = double.MinValue;
+
+    public void CheckMax(double value)
+    {
+        if (value > Max)
+            Max = value;
+    }
+}
+//4
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Random rnd = new Random();
+
+        double[] arr = new double[10];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.NextDouble() * 100;
+        }
+
+        SumCalculator sc = new SumCalculator();
+        MaxCalculator mc = new MaxCalculator();
+
+        ArrayProcessor.ProcessArray(arr, sc.AddToSum);
+        ArrayProcessor.ProcessArray(arr, mc.CheckMax);
+
+        Console.WriteLine("Array values:");
+        foreach (double v in arr)
+            Console.Write(v + " ");
+        Console.WriteLine();
+
+        Console.WriteLine("Sum = " + sc.Sum);
+        Console.WriteLine("Max = " + mc.Max);
+
+        //5
+        double[] arr2 = { 5, 12, 3, 9 };
+
+        double sum = 0;
+        double max = double.MinValue;
+
+        ArrayProcessor.ProcessArray(arr2, x => sum += x);
+
+        ArrayProcessor.ProcessArray(arr2, x =>
+        {
+            if (x > max)
+                max = x;
+        });
+
+        Console.WriteLine("Sum = " + sum);
+        Console.WriteLine("Max = " + max);
+
+    }
+}
---
+++ ex03/question6/Program.cs
@@ -0,0 +1,33 @@
+﻿public class OperationTable
+{
+    public delegate int BinaryOp(int a, int b);
+
+    private int startRow;
+    private int endRow;
+    private int startCol;
+    private int endCol;
+    private BinaryOp operation;
+
+    public OperationTable(int start_row, int end_row,
+                          int start_col, int end_col,
+                          BinaryOp op)
+    {
+        startRow = start_row;
+        endRow = end_row;
+        startCol = start_col;
+        endCol = end_col;
+        operation = op;
+    }
+
+    public void Print()
+    {
+        for (int i = startRow; i <= endRow; i++)
+        {
+            for (int j = startCol; j <= endCol; j++)
+            {
+                Console.Write(operation(i, j).ToString().PadLeft(5));
+            }
+            Console.WriteLine();
+        }
+    }
+}
